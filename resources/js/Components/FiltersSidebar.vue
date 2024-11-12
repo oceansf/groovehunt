ead967b3ba21705fc50c6de9623d4cb08b20544c
Ocean Fuaga <oceansfuaga@gmail.com>
2024-11-12 00:51:29 -0600

    Refactor filters sidebar into its own component

---
+++ resources/js/Components/FiltersSidebar.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { PlusIcon, MinusIcon } from "@heroicons/vue/24/outline";
+import FilterIcon from "./FilterIcon.vue";
+
+defineProps({
+    filters: Array,
+});
+</script>
+
+<template>
+    <div class="sticky top-[95px] pr-4 z-10 hidden h-screen overflow-y-auto lg:block">
+        <h2 class="hidden text-xl font-medium md:inline-block">Filters</h2>
+        <form>
+            <Disclosure
+                as="div"
+                v-for="section in filters"
+                :key="section.id"
+                class="border-b border-gray-200 py-6"
+                v-slot="{ open }"
+            >
+                <h3 class="-my-3 flow-root">
+                    <DisclosureButton
+                        class="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500"
+                    >
+                        <div class="flex items-center gap-2">
+                            <FilterIcon
+                                :icon="section.name.toLocaleLowerCase()"
+                                class="h-5 w-5"
+                                aria-hidden="true"
+                            />
+                            <span class="font-medium text-gray-900">{{
+                                section.name
+                            }}</span>
+                        </div>
+                        <span class="ml-6 flex items-center">
+                            <PlusIcon
+                                v-if="!open"
+                                class="h-5 w-5"
+                                aria-hidden="true"
+                            />
+                            <MinusIcon
+                                v-else
+                                class="h-5 w-5"
+                                aria-hidden="true"
+                            />
+                        </span>
+                    </DisclosureButton>
+                </h3>
+                <DisclosurePanel class="pt-6">
+                    <div class="space-y-4">
+                        <div
+                            v-for="(option, optionIdx) in section.options"
+                            :key="option.value"
+                            class="flex items-center"
+                        >
+                            <input
+                                :id="`filter-${section.id}-${optionIdx}`"
+                                :name="`${section.id}[]`"
+                                :value="option.value"
+                                type="checkbox"
+                                :checked="option.checked"
+                                class="h-4 w-4 cursor-pointer rounded border-gray-300 text-blue-500 focus:ring-blue-500"
+                            />
+                            <label
+                                :for="`filter-${section.id}-${optionIdx}`"
+                                class="ml-3 cursor-pointer text-sm text-gray-600"
+                            >
+                                {{ option.label }}</label
+                            >
+                        </div>
+                    </div>
+                </DisclosurePanel>
+            </Disclosure>
+        </form>
+    </div>
+</template>
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,29 +1,14 @@
 <script setup>
 import { ref } from "vue";
-import {
-    Dialog,
-    DialogPanel,
-    Disclosure,
-    DisclosureButton,
-    DisclosurePanel,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems,
-    TransitionChild,
-    TransitionRoot,
-} from "@headlessui/vue";
-import { XMarkIcon } from "@heroicons/vue/24/outline";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 import {
     ChevronDownIcon,
     FunnelIcon,
-    MinusIcon,
-    PlusIcon,
     ListBulletIcon,
 } from "@heroicons/vue/20/solid";
 import ListingsGrid from "@/Components/ListingsGrid.vue";
-import FilterIcon from "@/Components/FilterIcon.vue";
 import MobileFilterDialog from "@/Components/MobileFilterDialog.vue";
+import FiltersSidebar from "@/Components/FiltersSidebar.vue";
 
 defineProps({
     listings: Object,
@@ -118,6 +103,7 @@ const mobileFiltersOpen = ref(false);
                 v-model:mobileFiltersOpen="mobileFiltersOpen"
                 :filters="filters"
             />
+
             <main class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div
                     class="flex items-baseline justify-between border-b border-gray-200 px-3 pb-6 pt-12 md:px-0"
@@ -125,7 +111,7 @@ const mobileFiltersOpen = ref(false);
                     <h1 class="text-4xl font-bold tracking-tight text-gray-900">
                         Market
                     </h1>
-
+                    <!-- Sort & Filter controls -->
                     <div class="flex items-center">
                         <Menu as="div" class="relative inline-block text-left">
                             <div>
@@ -184,6 +170,7 @@ const mobileFiltersOpen = ref(false);
                                 aria-hidden="true"
                             />
                         </button>
+
                         <button
                             type="button"
                             class="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
@@ -202,86 +189,7 @@ const mobileFiltersOpen = ref(false);
                         class="grid grid-cols-1 gap-x-6 gap-y-10 lg:grid-cols-5"
                     >
                         <!-- Filters -->
-                        <div
-                            class="sticky top-[95px] z-10 hidden h-max lg:block"
-                        >
-                            <h2
-                                class="hidden text-xl font-medium md:inline-block"
-                            >
-                                Filters
-                            </h2>
-                            <form>
-                                <Disclosure
-                                    as="div"
-                                    v-for="section in filters"
-                                    :key="section.id"
-                                    class="border-b border-gray-200 py-6"
-                                    v-slot="{ open }"
-                                >
-                                    <h3 class="-my-3 flow-root">
-                                        <DisclosureButton
-                                            class="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500"
-                                        >
-                                            <div
-                                                class="flex items-center gap-2"
-                                            >
-                                                <FilterIcon
-                                                    :icon="
-                                                        section.name.toLocaleLowerCase()
-                                                    "
-                                                    class="h-5 w-5"
-                                                    aria-hidden="true"
-                                                />
-                                                <span
-                                                    class="font-medium text-gray-900"
-                                                    >{{ section.name }}</span
-                                                >
-                                            </div>
-                                            <span
-                                                class="ml-6 flex items-center"
-                                            >
-                                                <PlusIcon
-                                                    v-if="!open"
-                                                    class="h-5 w-5"
-                                                    aria-hidden="true"
-                                                />
-                                                <MinusIcon
-                                                    v-else
-                                                    class="h-5 w-5"
-                                                    aria-hidden="true"
-                                                />
-                                            </span>
-                                        </DisclosureButton>
-                                    </h3>
-                                    <DisclosurePanel class="pt-6">
-                                        <div class="space-y-4">
-                                            <div
-                                                v-for="(
-                                                    option, optionIdx
-                                                ) in section.options"
-                                                :key="option.value"
-                                                class="flex items-center"
-                                            >
-                                                <input
-                                                    :id="`filter-${section.id}-${optionIdx}`"
-                                                    :name="`${section.id}[]`"
-                                                    :value="option.value"
-                                                    type="checkbox"
-                                                    :checked="option.checked"
-                                                    class="h-4 w-4 cursor-pointer rounded border-gray-300 text-blue-500 focus:ring-blue-500"
-                                                />
-                                                <label
-                                                    :for="`filter-${section.id}-${optionIdx}`"
-                                                    class="ml-3 cursor-pointer text-sm text-gray-600"
-                                                >
-                                                    {{ option.label }}</label
-                                                >
-                                            </div>
-                                        </div>
-                                    </DisclosurePanel>
-                                </Disclosure>
-                            </form>
-                        </div>
+                        <FiltersSidebar :filters="filters" />
 
                         <!-- Product grid -->
                         <div class="lg:col-span-4">
