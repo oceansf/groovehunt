335541a95de3ee901161b27155eb62715144b1db
Ocean Fuaga <oceansfuaga@gmail.com>
2024-11-12 00:35:34 -0600

    Refactor mobile filter dialog into its own component

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -22,7 +22,8 @@ import {
     ListBulletIcon,
 } from "@heroicons/vue/20/solid";
 import ListingsGrid from "@/Components/ListingsGrid.vue";
-import Icon from "@/Components/FilterIcon.vue";
+import FilterIcon from "@/Components/FilterIcon.vue";
+import MobileFilterDialog from "@/Components/MobileFilterDialog.vue";
 
 defineProps({
     listings: Object,
@@ -113,161 +114,10 @@ const mobileFiltersOpen = ref(false);
 <template>
     <div class="bg-white">
         <div>
-            <!-- Mobile filter dialog -->
-            <TransitionRoot as="template" :show="mobileFiltersOpen">
-                <Dialog
-                    class="relative z-40 lg:hidden"
-                    @close="mobileFiltersOpen = false"
-                >
-                    <TransitionChild
-                        as="template"
-                        enter="transition-opacity ease-linear duration-300"
-                        enter-from="opacity-0"
-                        enter-to="opacity-100"
-                        leave="transition-opacity ease-linear duration-300"
-                        leave-from="opacity-100"
-                        leave-to="opacity-0"
-                    >
-                        <div class="fixed inset-0 bg-black bg-opacity-25" />
-                    </TransitionChild>
-
-                    <div class="fixed inset-0 z-40 flex">
-                        <TransitionChild
-                            as="template"
-                            enter="transition ease-in-out duration-300 transform"
-                            enter-from="translate-x-full"
-                            enter-to="translate-x-0"
-                            leave="transition ease-in-out duration-300 transform"
-                            leave-from="translate-x-0"
-                            leave-to="translate-x-full"
-                        >
-                            <DialogPanel
-                                class="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl"
-                            >
-                                <div
-                                    class="flex items-center justify-between px-4"
-                                >
-                                    <h2
-                                        class="text-lg font-medium text-gray-900"
-                                    >
-                                        Filters
-                                    </h2>
-                                    <button
-                                        type="button"
-                                        class="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
-                                        @click="mobileFiltersOpen = false"
-                                    >
-                                        <span class="sr-only">Close menu</span>
-                                        <XMarkIcon
-                                            class="h-6 w-6"
-                                            aria-hidden="true"
-                                        />
-                                    </button>
-                                </div>
-
-                                <!-- Filters -->
-                                <form
-                                    class="mt-4 overflow-y-auto border-t border-gray-200"
-                                >
-                                    <h3 class="sr-only">Filters</h3>
-                                    <ul
-                                        role="list"
-                                        class="px-2 py-3 font-medium text-gray-900"
-                                    >
-                                        <li
-                                            v-for="category in subCategories"
-                                            :key="category.name"
-                                        >
-                                            <a
-                                                :href="category.href"
-                                                class="block px-2 py-3"
-                                                >{{ category.name }}</a
-                                            >
-                                        </li>
-                                    </ul>
-
-                                    <Disclosure
-                                        as="div"
-                                        v-for="section in filters"
-                                        :key="section.id"
-                                        class="border-t border-gray-200 px-4 py-6"
-                                        v-slot="{ open }"
-                                    >
-                                        <h3 class="-mx-2 -my-3 flow-root">
-                                            <DisclosureButton
-                                                class="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500"
-                                            >
-                                                <div
-                                                    class="flex items-center gap-2"
-                                                >
-                                                    <Icon
-                                                        :icon="
-                                                            section.name.toLocaleLowerCase()
-                                                        "
-                                                        class="h-5 w-5"
-                                                        aria-hidden="true"
-                                                    />
-                                                    <span
-                                                        class="font-medium text-gray-900"
-                                                        >{{
-                                                            section.name
-                                                        }}</span
-                                                    >
-                                                </div>
-                                                <span
-                                                    class="ml-6 flex items-center"
-                                                >
-                                                    <PlusIcon
-                                                        v-if="!open"
-                                                        class="h-5 w-5"
-                                                        aria-hidden="true"
-                                                    />
-                                                    <MinusIcon
-                                                        v-else
-                                                        class="h-5 w-5"
-                                                        aria-hidden="true"
-                                                    />
-                                                </span>
-                                            </DisclosureButton>
-                                        </h3>
-                                        <DisclosurePanel class="pt-6">
-                                            <div class="space-y-6">
-                                                <div
-                                                    v-for="(
-                                                        option, optionIdx
-                                                    ) in section.options"
-                                                    :key="option.value"
-                                                    class="flex items-center"
-                                                >
-                                                    <input
-                                                        :id="`filter-mobile-${section.id}-${optionIdx}`"
-                                                        :name="`${section.id}[]`"
-                                                        :value="option.value"
-                                                        type="checkbox"
-                                                        :checked="
-                                                            option.checked
-                                                        "
-                                                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                                    />
-                                                    <label
-                                                        :for="`filter-mobile-${section.id}-${optionIdx}`"
-                                                        class="ml-3 min-w-0 flex-1 text-gray-500"
-                                                    >
-                                                        {{
-                                                            option.label
-                                                        }}</label
-                                                    >
-                                                </div>
-                                            </div>
-                                        </DisclosurePanel>
-                                    </Disclosure>
-                                </form>
-                            </DialogPanel>
-                        </TransitionChild>
-                    </div>
-                </Dialog>
-            </TransitionRoot>
-
+            <MobileFilterDialog
+                v-model:mobileFiltersOpen="mobileFiltersOpen"
+                :filters="filters"
+            />
             <main class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div
                     class="flex items-baseline justify-between border-b border-gray-200 px-3 pb-6 pt-12 md:px-0"
@@ -345,19 +195,21 @@ const mobileFiltersOpen = ref(false);
                     </div>
                 </div>
 
-                <section aria-labelledby="products-heading" class="pb-24 pt-6 ">
+                <section aria-labelledby="products-heading" class="pb-24 pt-6">
                     <h2 id="products-heading" class="sr-only">Listings</h2>
 
                     <div
-                    class="grid grid-cols-1 gap-x-6 gap-y-10 lg:grid-cols-5"
+                        class="grid grid-cols-1 gap-x-6 gap-y-10 lg:grid-cols-5"
                     >
-                    <!-- Filters -->
-                    <div
-                    class="sticky top-[95px] z-10 hidden h-max lg:block"
-                    >
-                    <h2 class="hidden text-xl font-medium md:inline-block">
-                        Filters
-                    </h2>
+                        <!-- Filters -->
+                        <div
+                            class="sticky top-[95px] z-10 hidden h-max lg:block"
+                        >
+                            <h2
+                                class="hidden text-xl font-medium md:inline-block"
+                            >
+                                Filters
+                            </h2>
                             <form>
                                 <Disclosure
                                     as="div"
@@ -373,7 +225,7 @@ const mobileFiltersOpen = ref(false);
                                             <div
                                                 class="flex items-center gap-2"
                                             >
-                                                <Icon
+                                                <FilterIcon
                                                     :icon="
                                                         section.name.toLocaleLowerCase()
                                                     "
@@ -433,7 +285,7 @@ const mobileFiltersOpen = ref(false);
 
                         <!-- Product grid -->
                         <div class="lg:col-span-4">
-                            <ListingsGrid :listings="listings"/>
+                            <ListingsGrid :listings="listings" />
                         </div>
                     </div>
                 </section>
